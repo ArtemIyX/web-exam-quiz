5bcf35aa3f83f7ddd0e4482fa0e3799c31e1a130
Wellsaik <1play2fun123@gmail.com>
2023-06-28 12:33:59 +0300

    Quiz creation, new way, only options

--- quiz-exam/public/js/quiz/createQuiz.js
+++ quiz-exam/public/js/quiz/createQuiz.js
@@ -1,133 +1,130 @@
-var questionCount = 0;
-
-
-function findQuestionDiv(questionNumber) {
-    const questionDivs = document.getElementsByClassName('question');
-
-    for (let i = 0; i < questionDivs.length; i++) {
-        const questionDiv = questionDivs[i];
-        const questionValue = questionDiv.getAttribute('question');
-
-        if (questionValue == questionNumber) {
-            return questionDiv;
-        }
-    }
-
-    return null;
-}
-
-function findOptionButton(questionNumber) {
-    const div = findQuestionDiv(questionNumber);
-    const btn = div.querySelector('.btn_add_option');
-    return btn;
-}
-
-function findMatchButton(questionNumber) {
-    const div = findQuestionDiv(questionNumber);
-    const btn = div.querySelector('.btn_add_match');
-    return btn;
-}
+// Initialize an empty array to store the questions
+var questions = [];
 
 function addQuestion() {
-    questionCount++;
+    // Create a new question object
+    var question = {
+        questionText: '',
+        answerOptions: [],
+        correctAnswer: null
+    };
 
-    var questionsContainer = document.getElementById('questionsContainer');
+    // Add the question to the array
+    questions.push(question);
 
-    var questionDiv = document.createElement('div');
-    questionDiv.classList.add('question');
-    questionDiv.setAttribute('question', questionCount);
-    questionDiv.innerHTML = '<h3>Question ' + questionCount + '</h3>';
+    // Render the question in the HTML
+    renderQuestion(question);
+}
 
-    var optionsDiv = document.createElement('div');
-    optionsDiv.id = 'options' + questionCount;
+function renderQuestion(question) {
 
-    var matchDiv = document.createElement('div');
-    matchDiv.id = 'match' + questionCount;
+    function renderAnswerOption(answerOption, answerOptionsContainer) {
+        // Create a new answer option container
+        var answerOptionContainer = document.createElement('div');
 
+        // Create a text input for the answer option text
+        var answerOptionText = document.createElement('input');
+        answerOptionText.type = 'text';
+        answerOptionText.placeholder = 'Enter answer option text';
+        answerOptionText.addEventListener('input', function () {
+            answerOption.text = answerOptionText.value;
+        });
+
+        // Create a checkbox to specify the correct answer
+        var correctAnswerCheckbox = document.createElement('input');
+        correctAnswerCheckbox.type = 'checkbox';
+        correctAnswerCheckbox.addEventListener('change', function () {
+            answerOption.isCorrect = correctAnswerCheckbox.checked;
+            // Uncheck all other checkboxes in the answer options
+            if (correctAnswerCheckbox.checked) {
+                for (var i = 0; i < answerOptionsContainer.children.length; i++) {
+                    var optionContainer = answerOptionsContainer.children[i];
+                    var checkbox = optionContainer.querySelector('input[type="checkbox"]');
+                    if (checkbox !== correctAnswerCheckbox) {
+                        checkbox.checked = false;
+                        questions[i].answerOptions.forEach(function (option) {
+                            option.isCorrect = false;
+                        });
+                    }
+                }
+            }
+        });
+
+        // Create a button to remove the answer option
+        var removeOptionButton = document.createElement('button');
+        removeOptionButton.textContent = 'Remove';
+        removeOptionButton.addEventListener('click', function () {
+            // Remove the answer option from the question
+            var index = question.answerOptions.indexOf(answerOption);
+            if (index !== -1) {
+                question.answerOptions.splice(index, 1);
+            }
+
+            // Remove the answer option container from the HTML
+            answerOptionContainer.remove();
+        });
+
+        // Append the answer option elements to the answer option container
+        answerOptionContainer.appendChild(answerOptionText);
+        answerOptionContainer.appendChild(correctAnswerCheckbox);
+        answerOptionContainer.appendChild(removeOptionButton);
+
+        // Append the answer option container to the answer options container
+        answerOptionsContainer.appendChild(answerOptionContainer);
+    }
+    
+    // Create a new question container
+    var questionContainer = document.createElement('div');
+
+    // Create a text input for the question text
+    var questionText = document.createElement('input');
+    questionText.type = 'text';
+    questionText.placeholder = 'Enter question text';
+    questionText.addEventListener('input', function () {
+        question.questionText = questionText.value;
+    });
+
+    // Create a button to delete the question
+    var deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', function () {
+        // Remove the question from the array
+        var index = questions.indexOf(question);
+        if (index !== -1) {
+            questions.splice(index, 1);
+        }
+
+        // Remove the question container from the HTML
+        questionContainer.remove();
+    });
+
+    // Create a button to add answer options
     var addOptionButton = document.createElement('button');
-    addOptionButton.type = 'button';
-    addOptionButton.innerText = 'Add Option';
-    addOptionButton.setAttribute('question', questionCount);
-    addOptionButton.classList.add('btn_add_option');
-    addOptionButton.addEventListener('click', function (event) {
-        const clickedButton = event.target;
-        const number = clickedButton.getAttribute('question');
-        addOption(number);
+    addOptionButton.textContent = 'Add Option';
+    addOptionButton.addEventListener('click', function () {
+        // Create a new answer option object
+        var answerOption = {
+            text: '',
+            isCorrect: false
+        };
+
+        // Add the answer option to the question
+        question.answerOptions.push(answerOption);
+
+        // Render the answer option in the HTML
+        renderAnswerOption(answerOption, answerOptionsContainer);
     });
 
-    var addMatchButton = document.createElement('button');
-    addMatchButton.type = 'button';
-    addMatchButton.innerText = 'Add Match';
-    addMatchButton.setAttribute('question', questionCount);
-    addMatchButton.classList.add('btn_add_match');
-    addMatchButton.addEventListener('click', function (event) {
-        const clickedButton = event.target;
-        const number = clickedButton.getAttribute('question');
-        addMatch(number);
-    });
+    // Create a container for answer options
+    var answerOptionsContainer = document.createElement('div');
 
-    questionDiv.appendChild(optionsDiv);
-    questionDiv.appendChild(matchDiv);
-    questionDiv.appendChild(addOptionButton);
-    questionDiv.appendChild(addMatchButton);
+    // Append the question elements to the question container
+    questionContainer.appendChild(questionText);
+    questionContainer.appendChild(deleteButton);
+    questionContainer.appendChild(addOptionButton);
+    questionContainer.appendChild(answerOptionsContainer);
 
-
-    questionsContainer.appendChild(questionDiv);
+    // Append the question container to the quiz container
+    quizContainer.appendChild(questionContainer);
 }
 
-function addOption(questionNumber) {
-    const optionsDiv = document.getElementById('options' + questionNumber);
-    const matchDiv = document.getElementById('match' + questionNumber);
-
-    if (matchDiv) {
-        matchDiv.remove();
-    }
-
-    const matchBtn = findMatchButton(questionNumber);
-    if(matchBtn) {
-        matchBtn.remove();
-    }
-
-    const optionLabel = document.createElement('label');
-    optionLabel.innerHTML = 'Option: ';
-
-    const optionInput = document.createElement('input');
-    optionInput.type = 'text';
-
-    const isCorrectLabel = document.createElement('label');
-    isCorrectLabel.innerHTML = 'Correct: ';
-
-    const isCorrectInput = document.createElement('input');
-    isCorrectInput.type = 'radio';
-    isCorrectInput.name = 'correctOption' + questionNumber;
-
-    optionsDiv.appendChild(optionLabel);
-    optionsDiv.appendChild(optionInput);
-    optionsDiv.appendChild(isCorrectLabel);
-    optionsDiv.appendChild(isCorrectInput);
-    optionsDiv.appendChild(document.createElement('br'));
-}
-
-function addMatch(questionNumber) {
-    var optionsDiv = document.getElementById('options' + questionNumber);
-    var matchDiv = document.getElementById('match' + questionNumber);
-
-    if (optionsDiv) {
-        optionsDiv.remove();
-    }
-
-    const optionButton = findOptionButton(questionNumber);
-    if(optionButton) {
-        optionButton.remove();
-    }
-
-    var matchLabel = document.createElement('label');
-    matchLabel.innerHTML = 'Match: ';
-
-    var matchInput = document.createElement('input');
-    matchInput.type = 'text';
-
-    matchDiv.appendChild(matchLabel);
-    matchDiv.appendChild(matchInput);
-    matchDiv.appendChild(document.createElement('br'));
-}
--- quiz-exam/resources/views/quiz/create.blade.php
+++ quiz-exam/resources/views/quiz/create.blade.php
@@ -2,48 +2,15 @@
 @section('title', 'Quiz Creation')
 
 @section('content')
+<h1>Quiz Creator</h1>
 
-
-<form id="quizForm">
-    <label for="quizTitle">Quiz Title:</label>
-    <input type="text" id="quizTitle" name="quizTitle">
-
-    <label for="quizDescription">Quiz Description:</label>
-    <textarea id="quizDescription" name="quizDescription"></textarea>
-
-    <div id="questionsContainer">
-        <!-- Questions will be dynamically added here -->
-    </div>
-
-    <button type="button" onclick="addQuestion()">Add Question</button>
-    <button type="submit">Submit</button>
-</form>
+  <div id="quizContainer">
+    <button onclick="addQuestion()">Add Question</button>
+  </div>
 
 <script src="{{asset('js/quiz/createQuiz.js')}}"></script>
 <script>
-            document.getElementById('quizForm').addEventListener('submit', function(event) {
-            event.preventDefault(); // Prevent form submission for this example
 
-            // Collect all the quiz data and process it
-            var formData = new FormData(this);
-            var quizData = {};
-
-            for (var pair of formData.entries()) {
-                var key = pair[0];
-                var value = pair[1];
-
-                if (quizData.hasOwnProperty(key)) {
-                    if (!Array.isArray(quizData[key])) {
-                        quizData[key] = [quizData[key]];
-                    }
-                    quizData[key].push(value);
-                } else {
-                    quizData[key] = value;
-                }
-            }
-
-            console.log(quizData);
-        });
 </script>
 
 @endsection
